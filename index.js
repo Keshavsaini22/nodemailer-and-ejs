56beb4949f95c5f6b1c0c7f84caebd4844d0c480
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-05-27 12:28:20 +0530

    general

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 8000
 const path = require('path')
 const nodemailer = require("nodemailer");
+const ejs = require('ejs')
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
@@ -22,35 +23,47 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-const mailOptions = {
-  from: {
-    name: 'Keshav Saini',
-    address: 'keshav.zenmonk@outlook.com'
-  }, // sender address
-  to: ["keshav.1147@zenmonk.tech"], // list of receivers
-  subject: "Hello ✔", // Subject line
-  text: "Testing", // plain text body
-  html: "<b>Hello world?</b>", // html body
-  // attachments: [
-  //   {
-  //     // filename: 'ccc.png',
-  //     // path: path.join(__dirname, 'ccc.png'),
-  //     // contentType: 'image/png'
-  //   }
-  // ]
+//Render the ejs template
+function sendEmailGeneral(to,subject){
+  ejs.renderFile(__dirname + '/views/welcome.ejs',{username:"Keshav"} ,function (err, template) {
+    if (err) {
+      console.log('err: ', err);
+    }
+    else {
+      const mailOptions = {
+        from: {
+          name: 'Keshav Saini',
+          address: 'keshav.zenmonk@outlook.com'
+        }, // sender address
+        to: [to], // list of receivers
+        subject: subject, // Subject line
+        text: "Testing", // plain text body
+        // html: "<b>Hello world?</b>", // html body
+        html: template, // html body
+  
+        // attachments: [
+        //   {
+        //     // filename: 'ccc.png',
+        //     // path: path.join(__dirname, 'ccc.png'),
+        //     // contentType: 'image/png'
+        //   }
+        // ]
+      }
+      const sendMail = async (transporter, mailOptions) => {
+        try {
+          await transporter.verify();
+          await transporter.sendMail(mailOptions);
+          console.log("success!")
+        } catch (error) {
+          console.log('error: ', error);
+        }
+      }
+      sendMail(transporter,mailOptions);
+    }
+  })
 }
 
-const sendMail = async (transporter, mailOptions) => {
-  try {
-    await transporter.verify();
-    await transporter.sendMail(mailOptions);
-    console.log("success!")
-  } catch (error) {
-    console.log('error: ', error);
-  }
-}
-
-// sendMail(transporter,mailOptions);
+// sendEmailGeneral("keshav.1147@zenmonk.tech","Hello Bhai ✔")
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
